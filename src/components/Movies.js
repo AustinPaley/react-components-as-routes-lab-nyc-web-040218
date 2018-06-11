9d34d8a865f82cfbef301e4c7c2b5f54ad41787a
AustinPaley <austinpaley32@gmail.com>
2018-06-11 16:49:58 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,20 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index) =>
+          <div key={index}>
+            <h3>{movie.title}</h3>
+            <p>Time: {movie.time}</p>
+            <ul>
+              <h4>Genres:</h4>
+            {movie.genres.map((genre, index) =>
+              <li key={index}>{genre}</li>
+            )}
+            </ul>
+            <p>{movie.metascore}</p>
+          </div>
+        )}
     </div>
   );
 };
